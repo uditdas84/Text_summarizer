87d5e9e1c1b5d93fe0c0233c18d850c704757ecc
uditdas84 <uditdas84@gmail.com>
2023-06-11 18:29:53 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools 
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ ="0.0.0"
+
+REPO_NAME = "Text_summarizer"
+AUTHOR_USER_NAME= "uditdas84"
+SRC_REPO="textsummarizer"
+AUTHOR_EMAIL="uditdas84@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url="https://github.com/"+AUTHOR_USER_NAME+"/"+SRC_REPO,
+    project_urls={
+        "Bug Tracker": "https://github.com/"+AUTHOR_USER_NAME+"/"+SRC_REPO+"/issues",
+    },
+    package_dir={"":"src"}, 
+    packages= setuptools.find_packages(where="src")
+
+)
