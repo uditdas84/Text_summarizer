04d08cc03a8d0c9549e621356c42c16926396530
uditdas84 <uditdas84@gmail.com>
2023-07-06 15:04:19 +0530

    data_ingestion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.logging import logger
 
-logger.info("Loading Text Summarizer")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} starteed <<<<")
+    data_inngestion = DataIngestionTrainingPipeline()
+    data_inngestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} complted <<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
