f08742c99cf829d9b42299ec7a80fe4343d51c5c
uditdas84 <uditdas84@gmail.com>
2023-07-07 15:53:12 +0530

    model_trainer added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainingPipeline
 from textsummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -30,6 +31,17 @@ try:
     logger.info(f">>>> stage {STAGE_NAME} starteed <<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>> stage {STAGE_NAME} complted <<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
+STAGE_NAME = "Model Training stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} starteed <<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>> stage {STAGE_NAME} complted <<<<")
 
 except Exception as e:
---
+++ src/textsummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,13 @@
+from textsummarizer.config.configuration import ConfigurationManager
+from textsummarizer.components.model_trainer import ModelTrainer
+from textsummarizer.logging import logger
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass 
+
+    def main(self):
+        config= ConfigurationManager()
+        model_trainer_config=config.get_model_trainer_config()
+        model_trainer = ModelTrainer(config=model_trainer_config)
+        model_trainer.train()
